7e952cda3c7b6d62609f455f9a88dd72739c1d42
张林烽 <linfeng.zhang@1hello.com>
2022-09-19 15:17:32 +0800

    update

--- src/main/java/dialog/ToolWindowDialog.java
+++ src/main/java/dialog/ToolWindowDialog.java
@@ -1,17 +1,21 @@
 package dialog;
 
 import com.intellij.openapi.ui.Messages;
+import com.intellij.ui.JBColor;
 import constant.COMMON_CONSTANT;
 import service.CreateFileService;
+import util.StringUtil;
 
 import javax.swing.*;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class ToolWindowDialog extends JDialog {
     private JPanel contentPane;
     private JButton buttonOK;
     private JTextArea textArea;
     private JTextField authorField;
-    private JTextField modularNameField;
+    private JTextField projectNameField;
     private JTextField packagePathField;
 
     public ToolWindowDialog() {
@@ -19,11 +23,29 @@ public class ToolWindowDialog extends JDialog {
         setModal(true);
         buttonOK.addActionListener(e -> {
             try {
-                new CreateFileService().createFile(authorField.getText(), modularNameField.getText(),packagePathField.getText(),textArea.getText());
-                Messages.showMessageDialog(COMMON_CONSTANT.SUCCESS, "", Messages.getInformationIcon());
+                new CreateFileService().createFile(authorField.getText(), projectNameField.getText(), packagePathField.getText(), textArea.getText());
+                Messages.showMessageDialog(COMMON_CONSTANT.SUCCESS, COMMON_CONSTANT.BLANK_STRING, Messages.getInformationIcon());
             } catch (Exception ex) {
                 ex.printStackTrace();
-                Messages.showMessageDialog(COMMON_CONSTANT.FAIL, "", Messages.getInformationIcon());
+                Messages.showMessageDialog(COMMON_CONSTANT.FAIL, COMMON_CONSTANT.BLANK_STRING, Messages.getInformationIcon());
+            }
+        });
+
+        projectNameField.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (COMMON_CONSTANT.PROJECT_INPUT_PLACEHOLDER.equals(projectNameField.getText())) {
+                    projectNameField.setText(COMMON_CONSTANT.BLANK_STRING);
+                    projectNameField.setForeground(JBColor.BLACK);
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (StringUtil.isEmpty(projectNameField.getText())) {
+                    projectNameField.setForeground(JBColor.GRAY);
+                    projectNameField.setText(COMMON_CONSTANT.PROJECT_INPUT_PLACEHOLDER);
+                }
             }
         });
     }
@@ -31,5 +53,4 @@ public class ToolWindowDialog extends JDialog {
     public JPanel getContent() {
         return this.contentPane;
     }
-
 }
