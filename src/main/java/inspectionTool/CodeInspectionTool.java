4b38af4a4c3ac14928781c4d8e1bbca9c4cfb414
张林烽 <linfeng.zhang@1hello.com>
2022-12-12 17:30:09 +0800

    无用代码删除

--- src/main/java/inspectionTool/CodeInspectionTool.java
+++ src/main/java/inspectionTool/CodeInspectionTool.java
@@ -2,17 +2,13 @@ package inspectionTool;
 
 import com.intellij.codeInspection.AbstractBaseJavaLocalInspectionTool;
 import com.intellij.codeInspection.ProblemsHolder;
-import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.JavaElementVisitor;
-import com.intellij.psi.PsiAnnotation;
-import com.intellij.psi.PsiAnnotationMemberValue;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.PsiField;
 import com.intellij.psi.PsiLocalVariable;
 import com.intellij.psi.PsiParameter;
 import com.intellij.psi.PsiType;
-import constant.ANNOTATION;
 import constant.TYPE;
 import org.jetbrains.annotations.NotNull;
 
@@ -43,30 +39,6 @@ public class CodeInspectionTool extends AbstractBaseJavaLocalInspectionTool {
                 checkType(holder, parameter.getTypeElement(), parameter.getType());
             }
 
-            @Override
-            public void visitAnnotation(PsiAnnotation annotation) {
-                super.visitAnnotation(annotation);
-                String qualifiedName = annotation.getQualifiedName();
-                if (null == qualifiedName) {
-                    return;
-                }
-                switch (qualifiedName) {
-                    case ANNOTATION.IBATIS_INSERT:
-                    case ANNOTATION.IBATIS_UPDATE:
-                    case ANNOTATION.IBATIS_SELECT:
-                    case ANNOTATION.IBATIS_DELETE:
-                        PsiAnnotationMemberValue memberValue = annotation.findAttributeValue(ANNOTATION.VALUE);
-                        if (null != memberValue) {
-                            String value = memberValue.getText();
-                            int index = value.indexOf("${");
-                            if (-1 != index) {
-                                holder.registerProblem(memberValue, new TextRange(index, index + 1), "Replace $ with #");
-                            }
-                        }
-                        break;
-                    default:
-                }
-            }
         };
     }
 
