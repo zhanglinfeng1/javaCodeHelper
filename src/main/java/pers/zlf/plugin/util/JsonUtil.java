71380bee091a9c8d67006e160e484e6c73f49205
张林烽 <linfeng.zhang@1hello.com>
2023-06-13 17:19:46 +0800

    fastjson2 -> Gson

--- src/main/java/pers/zlf/plugin/util/JsonUtil.java
+++ src/main/java/pers/zlf/plugin/util/JsonUtil.java
@@ -1,6 +1,7 @@
 package pers.zlf.plugin.util;
 
-import com.alibaba.fastjson2.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.Map;
 
@@ -9,17 +10,18 @@ import java.util.Map;
  * @Date create in 2022/9/18 11:44
  */
 public class JsonUtil {
+    private static final Gson gson = new GsonBuilder().create();
 
     public static Map<String, Object> toMap(Object obj) {
-        return toMap(JSONObject.toJSONString(obj));
+        return toMap(gson.toJson(obj));
     }
 
     public static Map<String, Object> toMap(String str) {
-        return JSONObject.parseObject(str, Map.class);
+        return gson.fromJson(str, Map.class);
     }
 
     public static <T> T toObject(String jsonString, Class<T> cls) {
-        return JSONObject.parseObject(jsonString, cls);
+        return gson.fromJson(jsonString, cls);
     }
 
 }
