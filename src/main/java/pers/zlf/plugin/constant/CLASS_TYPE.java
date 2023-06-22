8419ee8f6a44eb915bb9f3e6c3c52c4b345157c4
张林烽 <linfeng.zhang@1hello.com>
2023-06-22 09:16:34 +0800

    删除无用常量

--- src/main/java/pers/zlf/plugin/constant/CLASS_TYPE.java
+++ src/main/java/pers/zlf/plugin/constant/CLASS_TYPE.java
@@ -14,8 +14,6 @@ public class CLASS_TYPE {
     public static final String INTELLIJ_IDEA_RULEZZZ = "IntellijIdeaRulezzz";
     public static final String VOID = "void";
     public static final String CLASS_FILE_SUFFIX = ".class";
-    public static final String MAIN_FILE_SUFFIX = ".main";
-    public static final String TEST_FILE_SUFFIX = ".test";
     public static final List<String> BASIC_TYPE_LIST = Arrays.asList("int", "short", "long", "byte", "float", "double", "boolean", "char", "Integer", "Short", "Long", "Byte", "Float", "Double", "Boolean", "Character");
     public static final List<String> COMMON_TYPE_LIST = Arrays.asList("String", "Date", "Timestamp", "BigDecimal", "SimpleDateFormat", "Calendar", "LocalDate", "LocalTime", "LocalDateTime", "Logger");
 
