053bced3d21e7d4d945783a8850e37e16b4cecf0
张林烽 <linfeng.zhang@1hello.com>
2023-08-02 16:26:00 +0800

    删除无用常量

--- src/main/java/pers/zlf/plugin/constant/ClassType.java
+++ src/main/java/pers/zlf/plugin/constant/ClassType.java
@@ -12,7 +12,6 @@ public class ClassType {
     public static final String ARRAYS_PATH = "java.util.Arrays";
     public static final String INTELLIJ_IDEA_RULEZZZ = "IntellijIdeaRulezzz";
     public static final String VOID = "void";
-    public static final String CLASS_FILE = ".class";
     public static final String JAVA_FILE = ".java";
     public static final String XML_FILE = ".xml";
     public static final String FREEMARKER_FILE = ".ftl";
