ec528ad9378f265743aeb52601ba7466e965ce3b
张林烽 <linfeng.zhang@1hello.com>
2023-09-12 11:58:30 +0800

    优化

--- src/main/java/pers/zlf/plugin/inspection/OptionalInspection.java
+++ src/main/java/pers/zlf/plugin/inspection/OptionalInspection.java
@@ -8,6 +8,7 @@ import com.intellij.psi.PsiAssignmentExpression;
 import com.intellij.psi.PsiBinaryExpression;
 import com.intellij.psi.PsiBlockStatement;
 import com.intellij.psi.PsiCodeBlock;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.PsiExpression;
 import com.intellij.psi.PsiExpressionStatement;
@@ -17,6 +18,7 @@ import com.intellij.psi.PsiReferenceExpression;
 import com.intellij.psi.PsiStatement;
 import com.intellij.psi.PsiThrowStatement;
 import com.intellij.psi.impl.source.tree.java.PsiLiteralExpressionImpl;
+import com.intellij.psi.tree.IElementType;
 import org.jetbrains.annotations.NotNull;
 import pers.zlf.plugin.constant.ClassType;
 import pers.zlf.plugin.constant.Common;
@@ -29,6 +31,7 @@ import pers.zlf.plugin.util.lambda.Empty;
 
 import java.util.Optional;
 import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
 /**
@@ -36,42 +39,52 @@ import java.util.function.Predicate;
  * @date create in 2023/8/7 10:18
  */
 public class OptionalInspection extends BaseInspection {
-    /** if代码块的唯一语句 */
-    private PsiStatement ifCode;
+    /** 判断条件 */
+    private PsiExpression condition;
+    /** 代码块的唯一语句 */
+    private PsiStatement codeBlock;
+    /** 判断类型 */
+    private IElementType operationTokenType;
 
     @Override
     public @NotNull PsiElementVisitor buildVisitor(@NotNull ProblemsHolder holder, boolean isOnTheFly) {
         return new JavaElementVisitor() {
             @Override
             public void visitIfStatement(PsiIfStatement ifStatement) {
+                //快速解决方案
+                ReplaceQuickFix quickFix = new ReplaceQuickFix(Message.OPTIONAL_FIX_NAME, ifStatement, Common.BLANK_STRING);
                 //校验并解析if语句
-                PsiExpression judgmentObject = checkAndAnalysisIfStatement(ifStatement);
+                PsiExpression judgmentObject = checkAndAnalysisIfStatement(ifStatement, quickFix);
                 //判断的对象名
                 String variableName = Optional.ofNullable(judgmentObject).map(PsiExpression::getText).orElse(null);
                 if (StringUtil.isEmpty(variableName)) {
                     return;
                 }
-                //快速解决方案
-                ReplaceQuickFix quickFix = new ReplaceQuickFix(Message.OPTIONAL_FIX_NAME, ifStatement, Common.BLANK_STRING);
-                PsiFile psiFile = ifStatement.getContainingFile();
-                quickFix.addFixRunnable(() -> MyPsiUtil.importClass(psiFile, ClassType.OPTIONAL));
-                //简化判断对象的声明
-                simplifyDeclaration(judgmentObject);
-                //简化return
-                simplifyReturn(ifStatement, variableName);
                 //替换文本BiFunction
                 BiFunction<String, String, String> biFunction = null;
                 //替换文本前缀
                 String replacePrefix = Common.BLANK_STRING;
+                BiPredicate<Boolean, Boolean> simplifyCheck = (t, u) -> false;
                 //简化 throw
-                if (ifCode instanceof PsiThrowStatement) {
-                    biFunction = simplifyThrow((PsiThrowStatement) ifCode);
-                } else if (ifCode instanceof PsiExpressionStatement && (canSimplifyReturn || canSimplifyDeclaration)) {
-                    //赋值表达式、可以简化判断对象的声明或简化return
-                    biFunction = simplifyExpression((PsiExpressionStatement) ifCode, variableName);
+                if (codeBlock instanceof PsiThrowStatement) {
+                    biFunction = simplifyThrow((PsiThrowStatement) codeBlock);
+                    simplifyCheck = (t, u) -> true;
+                } else if (operationTokenType == JavaTokenType.EQEQ && codeBlock instanceof PsiExpressionStatement) {
+                    //判断类型为 == 、赋值表达式
+                    biFunction = simplifyExpression((PsiExpressionStatement) codeBlock, variableName);
                     replacePrefix = variableName + Common.EQ_STR;
+                    simplifyCheck = (t, u) -> t && u;
                 }
                 if (biFunction != null) {
+                    PsiFile psiFile = ifStatement.getContainingFile();
+                    quickFix.addFixRunnable(() -> MyPsiUtil.importClass(psiFile, ClassType.OPTIONAL));
+                    //简化判断对象的声明
+                    simplifyDeclaration(judgmentObject);
+                    //简化return
+                    simplifyReturn(ifStatement, variableName);
+                    if (!simplifyCheck.test(canSimplifyDeclaration, canSimplifyReturn)) {
+                        return;
+                    }
                     //简化声明
                     if (canSimplifyDeclaration) {
                         quickFix.addFixRunnable(declarationElement::delete);
@@ -79,43 +92,63 @@ public class OptionalInspection extends BaseInspection {
                         variableName = declarationRightText;
                     }
                     //简化return
-                    if (canSimplifyReturn) {
+                    if (canSimplifyReturn && operationTokenType == JavaTokenType.EQEQ) {
                         quickFix.addFixRunnable(returnStatement::delete);
                         replacePrefix = Keyword.JAVA_RETURN + Common.SPACE;
                     }
                     quickFix.setText(replacePrefix + biFunction.apply(variableName, Optional.ofNullable(simplifyReturnText).orElse(Common.BLANK_STRING)));
-                    holder.registerProblem(ifStatement, Message.OPTIONAL, ProblemHighlightType.WARNING, quickFix);
+                    holder.registerProblem(condition, Message.OPTIONAL, ProblemHighlightType.WARNING, quickFix);
                 }
             }
         };
     }
 
-    private PsiExpression checkAndAnalysisIfStatement(PsiIfStatement ifStatement) {
-        ifCode = null;
-        PsiExpression condition = ifStatement.getCondition();
-        //二元表达式，单个if，无else分支
-        boolean simpleIfStatement = condition instanceof PsiBinaryExpression && ifStatement.getParent() instanceof PsiCodeBlock && ifStatement.getElseBranch() == null;
+    private PsiExpression checkAndAnalysisIfStatement(PsiIfStatement ifStatement, ReplaceQuickFix quickFix) {
+        codeBlock = null;
+        condition = ifStatement.getCondition();
+        //二元表达式，单个if
+        boolean simpleIfStatement = condition instanceof PsiBinaryExpression && ifStatement.getParent() instanceof PsiCodeBlock;
         if (!simpleIfStatement) {
             return null;
         }
-        //获取if代码块，且只有一个表达式
+        PsiBinaryExpression binaryExpression = (PsiBinaryExpression) condition;
+        operationTokenType = binaryExpression.getOperationTokenType();
         PsiStatement thenStatement = ifStatement.getThenBranch();
-        if (thenStatement instanceof PsiBlockStatement) {
-            PsiBlockStatement blockStatement = (PsiBlockStatement) thenStatement;
-            ifCode = Optional.of(blockStatement.getCodeBlock().getStatements()).filter(t -> t.length == 1).map(t -> t[0]).orElse(null);
-            if (ifCode == null) {
+        PsiStatement elseStatement = ifStatement.getElseBranch();
+        PsiStatement codeStatement;
+        // 判断类型为 == 且 无else分支
+        if (operationTokenType == JavaTokenType.EQEQ && elseStatement == null) {
+            codeStatement = thenStatement;
+        } else if (operationTokenType == JavaTokenType.NE && elseStatement != null) {
+            // 判断类型为 != 且 存在else分支
+            codeStatement = elseStatement;
+            if (thenStatement instanceof PsiBlockStatement) {
+                PsiBlockStatement thenCodeBlock = (PsiBlockStatement) thenStatement;
+                for (PsiElement element : thenCodeBlock.getCodeBlock().getChildren()) {
+                    String text = element.getText();
+                    if (StringUtil.isEmpty(text) || Common.LEFT_BRACE.equals(text) || Common.RIGHT_BRACE.equals(text)) {
+                        continue;
+                    }
+                    quickFix.addPsiElement(element);
+                }
+            }
+        } else {
+            return null;
+        }
+        //代码块有且只有一个表达式
+        if (codeStatement instanceof PsiBlockStatement) {
+            PsiBlockStatement blockStatement = (PsiBlockStatement) codeStatement;
+            codeBlock = Optional.of(blockStatement.getCodeBlock().getStatements()).filter(t -> t.length == 1).map(t -> t[0]).orElse(null);
+            if (codeBlock == null) {
                 return null;
             }
-            PsiBinaryExpression binaryExpression = (PsiBinaryExpression) condition;
-            if (binaryExpression.getOperationTokenType() == JavaTokenType.EQEQ) {
-                PsiExpression leftOperand = binaryExpression.getLOperand();
-                PsiExpression rightOperand = binaryExpression.getROperand();
-                Predicate<PsiExpression> isNull = t -> (t instanceof PsiLiteralExpressionImpl && ((PsiLiteralExpressionImpl) t).getLiteralElementType() == JavaTokenType.NULL_KEYWORD);
-                if (isNull.test(leftOperand)) {
-                    return rightOperand;
-                } else if (isNull.test(rightOperand)) {
-                    return leftOperand;
-                }
+            PsiExpression leftOperand = binaryExpression.getLOperand();
+            PsiExpression rightOperand = binaryExpression.getROperand();
+            Predicate<PsiExpression> isNull = t -> (t instanceof PsiLiteralExpressionImpl && ((PsiLiteralExpressionImpl) t).getLiteralElementType() == JavaTokenType.NULL_KEYWORD);
+            if (isNull.test(leftOperand)) {
+                return rightOperand;
+            } else if (isNull.test(rightOperand)) {
+                return leftOperand;
             }
         }
         return null;
--- src/main/java/pers/zlf/plugin/inspection/fix/ReplaceQuickFix.java
+++ src/main/java/pers/zlf/plugin/inspection/fix/ReplaceQuickFix.java
@@ -10,6 +10,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 /**
  * @author zhanglinfeng
@@ -24,20 +25,23 @@ public class ReplaceQuickFix implements LocalQuickFix {
     private String text;
     /** 执行方法 */
     private final List<Runnable> runnableList;
+    /** 待添加的元素 */
+    private final List<PsiElement> addList;
 
     public ReplaceQuickFix(String name, PsiElement psiElement, String text) {
         this.text = text;
         this.psiElement = psiElement;
         this.name = name;
         this.runnableList = new ArrayList<>();
+        this.addList = new ArrayList<>();
     }
 
     public void setText(String text) {
         this.text = text;
     }
 
-    public String getText() {
-        return text;
+    public void addPsiElement(PsiElement psiElement) {
+        this.addList.add(psiElement);
     }
 
     public void addFixRunnable(Runnable runnable) {
@@ -58,9 +62,18 @@ public class ReplaceQuickFix implements LocalQuickFix {
 
     @Override
     public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
-        CommentTracker commentTracker = new CommentTracker();
-        PsiElement newElement = commentTracker.replaceAndRestoreComments(psiElement, text);
+        PsiElement parentElement = psiElement.getParent();
+        //添加元素
+        ListIterator<PsiElement> iterator = addList.listIterator(addList.size());
+        while (iterator.hasPrevious()) {
+            PsiElement element = iterator.previous();
+            parentElement.addAfter(element, psiElement);
+            CodeStyleManager.getInstance(project).reformat(element);
+        }
+        //替换
+        PsiElement newElement = new CommentTracker().replaceAndRestoreComments(psiElement, text);
         CodeStyleManager.getInstance(project).reformat(newElement);
+        //其他处理步骤
         runnableList.forEach(Runnable::run);
     }
 }
